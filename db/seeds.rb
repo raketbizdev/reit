b50d6dad0ebd811c5b7c62665bc2b8cd6bebee43
Ruel Nopal <radon1284@yahoo.com>
2019-03-15 01:15:55 +0800

    sdgsjdsg

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+CSV.foreach('db/seed/reit_content.csv', headers: true) do |row|
+	Company.create(
+	    company_name: row[0]
+	    directory_url: row[1]
+	    website: row[2]
+	    description: row[3]
+	    company_type: row[4]
+	    listing_status: row[5]
+	    industry_sector: row[6]
+	    investment_sector: row[7]
+	    exchange: row[8]
+	    ticker: row[9]
+	    address: row[10]
+	    phone: row[11]
+	    twitter: row[12]
+	    linkedin: row[13]
+	)
+
+	puts "Post: " + "#{$.}".to_s
+end
